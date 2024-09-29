c1a7260a7ad7a07180537e8fbb2c5026c2236934
Anonymousindex017 <ravi.kashyap181855@gmail.com>
2024-09-30 01:42:07 +0530

    singletable

--- createtable.java
+++ createtable.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 public class createtable {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number to print the table");
+        int num =s.nextInt();
 
         for(int i=1;i<=10;i++)
         {
-            if(i==5)
-            break;
-            System.out.println(i+" ");
+            System.out.println(num*i);
+        }
     }
-}
 }

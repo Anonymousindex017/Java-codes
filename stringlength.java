a99f533eda32bfa963616da1305b941ed322ce22
Anonymousindex017 <ravi.kashyap181855@gmail.com>
2024-10-05 14:38:42 +0530

    string length find

---
+++ stringlength.java
@@ -0,0 +1,9 @@
+public class stringlength {
+    public static void main(String[] args) {
+        String S1 = "Ravi";
+        String S2 = "Kashyap";
+        System.out.println("String is :"+S1.length());
+        System.out.println("String is : " +S2.length());
+        }
+    
+}

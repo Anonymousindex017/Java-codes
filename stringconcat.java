3c03e4161b7f5ccba96a168b144d9746d41be268
Anonymousindex017 <ravi.kashyap181855@gmail.com>
2024-10-05 14:46:32 +0530

    String concatenation

---
+++ stringconcat.java
@@ -0,0 +1,9 @@
+public class stringconcat {
+    public static void main(String[]args){
+        String S1="Ravi "; //String declaration
+        String S2="Kashyap"; //String declaration
+        System.out.println("String is : "+S1 + S2); //Simple concatenation
+        System.out.println("After the conctenation : "+S1.concat(S2)); //Using concat() method 
+    }
+    
+}

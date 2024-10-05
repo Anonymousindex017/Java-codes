745ab62dca0eb02b75f7ec36414479b672d34a4a
Anonymousindex017 <ravi.kashyap181855@gmail.com>
2024-10-05 14:52:56 +0530

    lower and upper case convert method

---
+++ upperandlowercase.java
@@ -0,0 +1,9 @@
+public class upperandlowercase {
+    public static void main (String[]args){
+        String S1= "Mera Naam Ravi Hai";  //String declaration
+        String S2 ="Mai Ek Dancer Hu";  //String declaration
+        System.out.println("Convert Upper case :"+S1.toUpperCase());  //Converts the string to upper case
+        System.out.println("Convert Lower case :"+S2.toLowerCase());  //Converts the string to lower case
+    }
+    
+}

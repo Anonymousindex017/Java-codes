c57ff91aad80cfa0bfed6922f2fe716c4e8cfd74
Anonymousindex017 <ravi.kashyap181855@gmail.com>
2025-03-12 08:02:34 +0530

    code prac

---
+++ PatternQuestion/tempCodeRunnerFile.java
@@ -0,0 +1,12 @@
+ public static void main(String[] args) {
+        
+        int i,j,n=10;
+        for(i=0;i<n;i++)
+        {
+            for(j=0; j<n-i-1;j++);
+            {
+                System.out.println("0");
+            }
+            System.out.println(n-i);
+        }
+    }
